8aa620dcedafe9cda1d58f289aa5d0714117a179
00LiRika00 <45798205+00LiRika00@users.noreply.github.com>
2018-12-21 11:43:13 +0200

    Hello

--- изначальный Bus/sendData/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SenddataConfig(AppConfig):
-    name = 'sendData'
--- изначальный Bus/sendData/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-
-def data(request):
-    return HttpResponse("<h1>Data is here<h1>")
